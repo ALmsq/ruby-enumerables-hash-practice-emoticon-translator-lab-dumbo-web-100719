6404e427a45565ad9cd73dafbce7942a82ecfba5
Alexander Mosquea <alexander.rob0@gmail.com>
2019-09-28 05:51:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,49 +2,49 @@ require "yaml"
 
 
 def load_library(emotes)
-  #emoticons = YAML.load_file(emotes)
+  # #emoticons = YAML.load_file(emotes)
   
+  # # hash = {
+  # #   :get_meaning => {
+  # #     "☜(⌒▽⌒)☞" => "angel",
+  # #     "ヽ(ｏ`皿′ｏ)ﾉ" => "angry",
+  # #     "(ΘεΘ;)" => "bored",
+  # #     "(゜.゜)" => "confused",
+  # #     "(#^.^#)" => "embarrased",
+  # #     ">゜))))彡" => "fish",
+  # #     "(^0_0^)" => "glasses",
+  # #     "(￣ー￣)" => "grinning",
+  # #     "(＾ｖ＾)" => "happy",
+  # #     "(*^3^)/~☆" => "kiss",
+  # #     "(Ｔ▽Ｔ)" =>  "sad",
+  # #     "o_O" => "surprised",
+  # #     "(^_-)" => "wink"
+  # #   },
+  # #   :get_emoticon => {
+  # #     "O:)" => "☜(⌒▽⌒)☞",
+  # #     ">:(" => "ヽ(ｏ`皿′ｏ)ﾉ",
+  # #     ":O" => "(ΘεΘ;)",
+  # #     "%)" => "(゜.゜)",
+  # #     ":$" => "(#^.^#)",
+  # #     "><>" => ">゜))))彡",
+  # #     "8D" => "(^0_0^)",
+  # #     "=D" => "(￣ー￣)",
+  # #     ":)" => "(＾ｖ＾)",
+  # #     ":*" => "(*^3^)/~☆",
+  # #     ":'(" => "(Ｔ▽Ｔ)",
+  # #     ":o" => "o_O",
+  # #     ";)" => "(^_-)"
+  # #   }
+  # # }
   # hash = {
-  #   :get_meaning => {
-  #     "☜(⌒▽⌒)☞" => "angel",
-  #     "ヽ(ｏ`皿′ｏ)ﾉ" => "angry",
-  #     "(ΘεΘ;)" => "bored",
-  #     "(゜.゜)" => "confused",
-  #     "(#^.^#)" => "embarrased",
-  #     ">゜))))彡" => "fish",
-  #     "(^0_0^)" => "glasses",
-  #     "(￣ー￣)" => "grinning",
-  #     "(＾ｖ＾)" => "happy",
-  #     "(*^3^)/~☆" => "kiss",
-  #     "(Ｔ▽Ｔ)" =>  "sad",
-  #     "o_O" => "surprised",
-  #     "(^_-)" => "wink"
-  #   },
-  #   :get_emoticon => {
-  #     "O:)" => "☜(⌒▽⌒)☞",
-  #     ">:(" => "ヽ(ｏ`皿′ｏ)ﾉ",
-  #     ":O" => "(ΘεΘ;)",
-  #     "%)" => "(゜.゜)",
-  #     ":$" => "(#^.^#)",
-  #     "><>" => ">゜))))彡",
-  #     "8D" => "(^0_0^)",
-  #     "=D" => "(￣ー￣)",
-  #     ":)" => "(＾ｖ＾)",
-  #     ":*" => "(*^3^)/~☆",
-  #     ":'(" => "(Ｔ▽Ｔ)",
-  #     ":o" => "o_O",
-  #     ";)" => "(^_-)"
-  #   }
+  #   :get_meaning => {},
+  #   :get_emoticon => {},
   # }
-  hash = {
-    :get_meaning => {},
-    :get_emoticon => {},
-  }
-  YAML.load_file(emotes).each {|key, value|
-    hash[:get_meaning][jap_emote] = key
-    hash[:get_emoticon][eng_emote] = value
-  }
-  hash
+  # YAML.load_file(emotes).each {|key, value|
+  #   hash[:get_meaning][jap_emote] = key
+  #   hash[:get_emoticon][eng_emote] = value
+  # }
+  # hash
 end
 
 def get_japanese_emoticon(path, emoticon)
